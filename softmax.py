3ec3a5405cc01c952e402e7818caeccf2beef35e
Usama Hussain <uhussain@wisc.edu>
2018-11-26 15:01:52 +0100

    Simple Softmax function to turn scores y into probabilities S(y)

---
+++ softmax.py
@@ -0,0 +1,23 @@
+"""Softmax."""
+
+
+import numpy as np
+import matplotlib
+
+scores = np.array([3.0, 1.0, 0.2])
+def softmax(x):
+    """Compute softmax values for each sets of scores in x."""
+      # TODO: Compute and return softmax(x)
+    return (np.exp(x)/np.sum(np.exp(x),axis=0))
+print (scores/10)
+print(softmax(scores/10))
+
+matplotlib.use('agg')
+# Plot softmax curves
+import matplotlib.pyplot as plt
+x = np.arange(-2.0, 6.0, 0.1)
+scores = np.vstack([x, np.ones_like(x), 0.2 * np.ones_like(x)])
+
+plt.plot(x, softmax(scores).T, linewidth=2)
+plt.show()
+
